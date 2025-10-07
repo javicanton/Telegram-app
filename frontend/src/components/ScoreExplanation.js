61adb6ffba4bdc4456f4e015ef24f6ad517e63b4
Javier Cantón <prosumidor.social@gmail.com>
2025-10-07 14:57:14 +0200

    fix: Limpiar advertencias de ESLint
    - Remover import no utilizado de useState en App.js
    - Remover import no utilizado de Box en ScoreExplanation.js
    - Build más limpio sin advertencias

--- frontend/src/components/ScoreExplanation.js
+++ frontend/src/components/ScoreExplanation.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
 
 function ScoreExplanation() {
   return (
